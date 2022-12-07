eb99a20370f7017def9fd16a1b46553438d13b6b
Herin <119970820+herinsaharan@users.noreply.github.com>
2022-12-07 19:47:17 +0530

    Update ChangepassJS.js

--- ChangepassJS.js
+++ ChangepassJS.js
@@ -3,7 +3,7 @@ var password = document.getElementById("password")
 
 
 enableSubmitButton();
-
+// Name.txt
 function validatePassword() {
   if(password.value != confirm_password.value) {
     confirm_password.setCustomValidity("Passwords Don't Match");
